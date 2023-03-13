2cb8b4db83fc8f8cb6a319b434cbed81353bea19
dev-mkr <webdev.mu@gamil.com>
2023-03-13 20:13:07 +0200

    add: icons to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,26 @@
 import Link from "next/link";
 import Logo from "public/logo.svg";
-
+import HartIcon from "public/heart-icon.svg";
+import ShoppingCartIcon from "public/shopping-cart-icon.svg";
+import DarkThemeIcon from "public/dark-theme-icon.svg";
+//TODO remove each icon to it's own file to achieve solid
 const Header = () => {
   return (
     <header className="container sticky top-0 flex justify-between pt-2">
-      <Link href="/" className="flex items-center">
-        <Logo alt="logo" className="w-8 sm:w-12" />
-        <span className="font-bold">All-One</span>
+      <Link href="/" className="flex items-center gap-x-1">
+        <Logo alt="logo" className="w-10 sm:w-12" />
+        <span className="font-extrabold">All-One</span>
       </Link>
-      <ul className="flex items-center gap-x-5">
-        <li>loved</li>
-        <li>cart</li>
-        <li>switch</li>
+      <ul className="flex items-center gap-x-5 [&>li]:w-7">
+        <li>
+          <HartIcon className="" />
+        </li>
+        <li>
+          <DarkThemeIcon />
+        </li>
+        <li>
+          <ShoppingCartIcon />
+        </li>
       </ul>
     </header>
   );
